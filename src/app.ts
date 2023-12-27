adfcb8ac44079b80af7c63ed621bca6b06fff3da
Lennon Araujo <lennon_hih@hotmail.com>
2023-12-26 21:55:28 -0300

    chore: error handling - zoderror format

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'express-async-errors'
 import express, { NextFunction, Request, Response } from 'express'
 import { router } from './shared/http/routes'
 import { AppError } from './shared/errors/AppError'
+import { ZodError } from 'zod'
 
 const app = express()
 app.use(express.json())
@@ -11,6 +12,16 @@ app.use(router)
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ message: err.message })
+  }
+
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .json({ message: 'Validation error.', issues: err.format() })
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.error(err)
   }
 
   return res.status(500).json({
